1a5744dde702249baf84016fdf1e6a3da25d83a2
Fisherman386 <carlospuenteg@gmail.com>
2022-01-24 17:38:40 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,84 +1,102 @@
 import random
+import os
 
 ######################################################################
 
 wins = [ [0,1,2] , [3,4,5] , [6,7,8] , [0,3,6] , [1,4,7] , [2,5,8] , [0,4,8] , [3,5,7] ]
-gamesWon = []
-wonNum = 0
-data = eval(open("data.txt", "r").read())
 
 ######################################################################
 
 def play():
-    global gamesWon, wonNum
-    found = False
-    winner = 0
-
-    game = []
+    global won, lost
     locations = [0,1,2,3,4,5,6,7,8]
+    game = []
 
-    for i in range(9):
-        found = False
+    for i in range(9): # 9 are the max turns
+        winLocs = []
+        lostLocs = []
 
-        if i%2:  # If AI is playing  0
-            if data:
-                for x in data:
-                    if len(x) > (i+1) and x[:i] == game and x[i] in locations:  # If the AI has a game that started like this, copy it
-                        game.append(x[i])
-                        locations.remove(x[i])
-                        found = True
-            if not found:
-                r = random.choice(locations)
+        if i % 2:  # If it's AI's turn
+            for w in won:
+                if  w[:i] == game  and  len(w) > i  and  w[i] not in winLocs:
+                    winLocs.append(w[i])
+
+            for l in lost:
+                if  l[:i] == game  and  len(l) > i  and  l[i] not in lostLocs:
+                    lostLocs.append(l[i])
+            
+            winLocs = [x for x in winLocs if x not in lostLocs and x in locations]
+
+            if winLocs:
+                r = random.choice(winLocs)
+                game.append(r)
+                locations.remove(r)
+            else:
+                locs = [x for x in locations if x not in lostLocs]
+                if locs:
+                    r = random.choice(locs)
+                else:
+                    r = random.choice(locations)
                 game.append(r)
                 locations.remove(r)
 
-        else:  # If user is playing
+        else:
             r = random.choice(locations)
             game.append(r)
             locations.remove(r)
-        
+
         player1 = game[0::2]
         player2 = game[1::2]
         
         for w in wins:
             if all(elem in player1 for elem in w):
-                winner = 1
-                break
+                if game not in won:
+                    won.append(game)
+                return 1
             elif all(elem in player2 for elem in w):
-                winner = 2
-                break
+                if game not in lost:
+                    lost.append(game)
+                return 0
 
-    if winner == 1:
-        wonNum += 1
-        #if game not in data and game not in gamesWon:
-        gamesWon.append(game)
+    return 0
 
 ######################################################################
 
-def train(tries):
-    global data, gamesWon
-    for _ in range(tries//100):
-        gamesWon = []
-        data = eval(open("data.txt", "r").read())
-        for _ in range(200):
-            play()
-        
-        data += gamesWon
-        f = open("data.txt", "w")
-        f.write(str(data))
-        f.close()
+def train():
+    global won, lost
+    createFiles()
+
+    totalWins = 0
+    won = eval(open("won.txt", "r").read())
+    lost = eval(open("lost.txt", "r").read())
+
+    for t in range(times):
+        totalWins += play()
+
+    wonFile = open("won.txt", "w")
+    wonFile.write(str(won))
+    wonFile.close()
+
+    lostFile = open("lost.txt", "w")
+    lostFile.write(str(lost))
+    lostFile.close()
+
+    return totalWins
 
 ######################################################################
 
-def goodness():
-    global data
-    data = eval(open("data.txt", "r").read())
-    for _ in range(100):
-        play()
-        
-    print(wonNum)
+def createFiles():
+    if "won.txt" not in os.listdir(os.getcwd()):
+        wonFile = open("won.txt", "w")
+        wonFile.write("[]")
+        wonFile.close()
+
+    if "lost.txt" not in os.listdir(os.getcwd()):
+        lostFile = open("lost.txt", "w")
+        lostFile.write("[]")
+        lostFile.close()
 
 ######################################################################
 
-#train(1000000)
-goodness()
+for _ in range(1000):
+    print(train(100))
